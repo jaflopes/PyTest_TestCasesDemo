785e84c8ef396a64b0879c68fd9dbc78f1b46ee1
Joao Andre Farias Lopes <lopes.jaf@gmail.com>
2021-04-28 08:45:09 -0700

    Add files via upload

--- PyTestFirstTestCaseCar/car.py
+++ PyTestFirstTestCaseCar/car.py
@@ -35,21 +35,19 @@ if __name__ == '__main__':
         try:
             action = input("What should I do? [A]ccelerate, [B]rake, " 
                            "show [O]dometer, or show average [S]peed?").upper()
-        
-            if action not in "ABOS" or len(action) != 1:
-                print("I don't know how to do that")
-                continue
-            if action == 'A':
-                my_car.accelerate()
-            elif action == 'B':
-                my_car.brake()
-            elif action == 'O':
-                print("The car has driven {} kilometers".format(my_car.odometer))
-            elif action == 'S':
-                print("The car's average speed was {} kph".format(my_car.average_speed()))
-            
-            my_car.step()
-            my_car.say_state()
-        
         except EOFError as e:
             print(e)
+
+        if action not in "ABOS" or len(action) != 1:
+            print("I don't know how to do that")
+            continue
+        if action == 'A':
+            my_car.accelerate()
+        elif action == 'B':
+            my_car.brake()
+        elif action == 'O':
+            print("The car has driven {} kilometers".format(my_car.odometer))
+        elif action == 'S':
+            print("The car's average speed was {} kph".format(my_car.average_speed()))
+        my_car.step()
+        my_car.say_state()
